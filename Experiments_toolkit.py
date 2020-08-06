23a2baa07a8b4d94d48b72afb102b428aacd83fd
Maddy Armstrong <maddya@google.com>
2020-08-06 23:44:30 +0000

    requested changes

--- Experiments_toolkit.py
+++ Experiments_toolkit.py
@@ -20,9 +20,9 @@ def create_train_test_validation(positives, negatives, percent_train, percent_te
             test -- dict (Dictionary with percent_test of original dictionary)
             validation -- dict (Dictionary with percent_validation of original dictionary)
         """
-    train = {"postivies": [], "negatives": []}
-    test = {"postivies": [], "negatives": []}
-    validation = {"postivies": [], "negatives": []}
+    train = {"positives": [], "negatives": []}
+    test = {"positives": [], "negatives": []}
+    validation = {"positives": [], "negatives": []}
 
     if (percent_train + percent_test + percent_validation != 1):
         print("ERROR: percentages don't add to 1")
@@ -65,7 +65,7 @@ def splitting_up_data(data, key, train, percent_train, test, percent_test, valid
 
 def preprocess_data(data, rows, filtering_params, window_function, window_size, offset):
     """
-       Description: Preprocess the data stored in data by windowing the traces and saving
+       Description: Process the samples stored in data by windowing the traces and saving
                                 these windows (numpy arrays) into a dictionary.
 
        Args:
@@ -76,7 +76,7 @@ def preprocess_data(data, rows, filtering_params, window_function, window_size, 
                                 stores important parameters for the 2 steps involved with 
                                 finding the precise start and end time of a activity in the data)
             window_size -- int (Window size of the trace to be saved into the
-            				    dictionary that will be returned)
+            				    numpy array windows that will be returned)
             offset -- int (Number to add to the predicted start time of the 
             			   activity from where the window will start at)
 
@@ -84,8 +84,7 @@ def preprocess_data(data, rows, filtering_params, window_function, window_size, 
            windows-- int[] (Array of the windows cropped by the window_function)
            """
     windows = []
-    for j in range(len(data)):
-        sample = data[j]
+    for sample in data:
         activity_windows = wl.initial_find_activity(
             sample, rows, filtering_params.initial_filtering.window_size,
             filtering_params.initial_filtering.stride,
@@ -95,11 +94,11 @@ def preprocess_data(data, rows, filtering_params, window_function, window_size, 
                 activity_windows, rows, filtering_params.precise_filtering.window_size,
                 filtering_params.precise_filtering.stride,
                 filtering_params.precise_filtering.threshold)
-            pos_window = window_function(
+            windowed_trace = window_function(
                 sample, start_time + offset, window_size)
-            if len(pos_window[0][0]) != window_size:
+            if len(windowed_trace[0][0]) != window_size:
                 continue
-            windows.extend(pos_window)
+            windows.extend(windowed_trace)
     np_windows = np.array(windows)
     return np_windows
 
@@ -107,7 +106,7 @@ def preprocess_data(data, rows, filtering_params, window_function, window_size, 
 def get_np_X_Y(x, y, length):
     """
        Description: Create X and Y numpy arrays desired length of random 
-       				samples from lists of windowed data, x and y
+       				samples (without replacement) from lists of windowed data, x and y
 
        Args:
            x -- np_array (Numpy array of one category numpy arrays with the same shape)
@@ -175,17 +174,16 @@ def preprocess_add_augmentation(data, augmentation_function, sigma):
            pos_key -- string (Key in dictionary to store augmented windows) 
            augmentation_function -- function (Desired augmentation function of the form:
                                               augment(np_array data, int sigma))
-           sigma -- double (Level of augment to be passed to the augmentation function )
+           sigma -- double (Level of augmentation to be passed to the augmentation function )
            size -- int (Window size of traces to be stored in dictionary)
 
        Return:
            dictionary -- dict (Dictionary with augmented windows of size = size in key pos_key)
      """
     augmented = []
-    for j in range(len(data)):
-        sample = data[j]
-        sample = sample[:-1]
-        transposed = sample.transpose()
+    for sample in data:
+        sample_no_timeseries = sample[:-1]
+        transposed = sample_no_timeseries.transpose()
         augmented_trace = augmentation_function(transposed, sigma)
         augmented_trace = augmented_trace.transpose()
         augmented.extend([augmented_trace])
